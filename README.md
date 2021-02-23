ac004588d5d0d88b9ce49df60ebcbd9df07527d0
Eduardo Cassinelli <edujkb@gmail.com>
2021-02-23 15:39:39 -0500

    Added a python script to get a Plotly Dashboard and modified README.md file.

--- README.md
+++ README.md
@@ -4,10 +4,13 @@ the game Call of Duty Warzone/
 
 The script takes a players_info.csv dataset, that contains information of several players, as an input, extracts and transforms the data and loads it into the SQL databse. 
 
+BONUS: simple dinamic Plotly dashboard to visualize barplots to compare players in each stat. 
+
 ## Contents
 
 ```bash
 .
+├── cod_dashboard.py
 ├── cod_tracker.py
 ├── datasets
 │   ├── players_info.csv
---
+++ cod_dashboard.py
@@ -0,0 +1,39 @@
+import dash
+import dash_core_components as dcc 
+import dash_html_components as html 
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go 
+import plotly.express as px
+import pandas as pd
+
+stats_df = pd.read_csv('./datasets/players_stats.csv')
+list_of_stats = ['wins', 'kd_ratio', 'kills', 'deaths', 'downs', 'damage', 'level']
+
+app = dash.Dash()
+
+app.layout = html.Div([
+    html.H1('Cod/WZ Leaderboard Dashboard', style={'textAlign':'center'}),
+    html.Div([
+        dcc.Graph(id='graph'),
+        dcc.Dropdown(id='stat_selector',
+                    options=[{'label':stat_name.capitalize(),'value':stat_name} for stat_name in list_of_stats],
+                    value='wins')
+    ])
+])
+
+@app.callback(Output(component_id='graph', component_property='figure'),
+            [Input(component_id='stat_selector', component_property='value')])
+def update_figure(selected_stat):
+    df_by_stat = stats_df[['player', selected_stat]]
+    
+    data = [go.Bar(x=df_by_stat['player'],
+                    y=df_by_stat[selected_stat]
+    )]
+
+    layout = go.Layout(title=selected_stat.capitalize() + ' ' + 'in Warzone BR Mode by player')
+
+    return {'data': data, 'layout': layout}
+
+
+if __name__ == '__main__':
+    app.run_server()
